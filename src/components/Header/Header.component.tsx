4f4ea14a37dd7a0187b745f7aef5d029b16181e1
Marcelo Bracet <marcelo.bracet1@gmail.com>
2024-04-17 18:10:20 -0300

    feat: first commit

---
+++ src/components/Header/Header.component.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { motion as m } from "framer-motion";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return <m.header className={styles.headerContainer}>Header</m.header>;
+}
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,12 @@
+import Header from "@/components/Header/Header.component";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head />
       <body>
+        <Header />
         <Main />
         <NextScript />
       </body>
